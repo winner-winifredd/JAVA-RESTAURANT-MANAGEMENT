2f9b260a46db1b26d8795f33d0dc77df2c479c67
highoncodes1 <odilichekwubewinifred@gmail.com>
2023-06-09 11:12:34 +0100

    Initial commit

---
+++ RMS.java
@@ -0,0 +1,7 @@
+public class RMS
+{
+	public static void main(String[] args) {
+		Controller cController = new Controller();
+		cController.mainLoop();
+	}
+}
---
+++ RMS_GUI.java
@@ -0,0 +1,9 @@
+
+public class RMS_GUI
+{
+	public static void main(String[] args) {
+		Controller_GUI cController = new Controller_GUI();
+		//cController.mainLoop();
+	}
+}
+
